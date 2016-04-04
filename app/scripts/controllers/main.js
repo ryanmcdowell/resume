99b053fcd9e9eb7398ef4a20c763edc6d18e07bd
McDowell, Ryan <Ryan.McDowell@nb.com>
2016-04-04 10:21:42 -0500

    Add listener for window size changes so the resume boxes auto-expand when the site is being displayed as mobile site.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-angular.module('resumeApp').controller('MainCtrl', function () {
+angular.module('resumeApp').controller('MainCtrl', function ($scope, $window) {
 
-});
+    /****************** Scope Functions **********************/
+
+    /**
+     * Expands the resume boxes when the window size
+     * is less than 767 (e.g. the site is being displayed
+     * as a mobile version).
+     *
+     * @param  {Integer} newSize    The new size of the window.
+     */
+    $scope.changeWindowSize = function (newSize) {
+
+      if (newSize <= 767) {
+        $scope.showWork1 = true;
+        $scope.showWork2 = true;
+        $scope.showWork3 = true;
+      }
+    };
+
+    /****************** Initialization **********************/
+
+    // Bind an event listener so the change function is invoked
+    // whenever the window is resized.
+    angular.element($window).bind('resize', function () {
+      $scope.changeWindowSize($window.innerWidth);
+    });
+
+    // Invoke the changeWindowSize once on load so the
+    // page displays in the correct format.
+    $scope.changeWindowSize($window.innerWidth);
+
+  });
