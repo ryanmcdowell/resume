a65b370d13f0eb3db2179ec87e7f9143fac86045
Ryan McDowell <me@ryanmcdowell.io>
2016-06-19 00:34:35 -0500

    fix: Update deploy.sh so the version bump happens prior to the deploy and is rolled back in the event of an error.

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,8 @@ fi
 PROJECT_ID=resume-1270
 
 # Parse the version out of the package.json so we can automatically deploy a
-# release with the version number in the release name. The below command
+# release with the version number in the release name and store the old version
+# number in case the deploy fails and the bump needs to be rolled back. The below command
 # requires the command-line JSON processor "jq" to be installed.
 #
 # https://github.com/stedolan/jq
@@ -34,8 +35,13 @@ PROJECT_ID=resume-1270
 # If jq is not installed, you can use the below as an alternative which uses
 # python and bash to parse the version number.
 #
-# VERSION=`cat package.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["version"];' | tr -d '.'`
+# OLD_VERSION=`cat package.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["version"];'`
 #
+OLD_VERSION=`cat package.json | jq -r '.version'`
+
+# Bump the version for the next release.
+printf "Bumping version for next release...\n"
+grunt bump-only
 VERSION=`cat package.json | jq -r '.version' | tr -d '.'`
 
 printf "\n\n"
@@ -47,6 +53,8 @@ gcloud preview app deploy --project $PROJECT_ID --version release-$VERSION
 # Stop execution on failure.
 if [[ "$?" != 0 ]]
 then
+    # Reset the version numbers.
+    grunt bump-only --setversion=$OLD_VERSION
     exit 1
 fi
 
@@ -63,14 +71,5 @@ fi
 # commit the release to source control.
 grunt conventionalChangelog
 grunt bump-commit
-
-# Bump the version for the next release.
-printf "Bumping version for next release...\n"
-grunt bump-only
-
-
-
-
-
 
 
