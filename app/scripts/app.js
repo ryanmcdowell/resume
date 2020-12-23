2bcb26c3e6b14b8736947dc51bdc4b330dff4e08
Ryan McDowell <me@ryanmcdowell.io>
2020-12-23 14:29:25 -0600

    feat: Remove hashtag from URL & use HTML5 History API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ angular.module('resume', [
   'ui.bootstrap',
   'base64'
 ])
-  .config(function ($routeProvider, $compileProvider, Config) {
+  .config(function ($routeProvider, $locationProvider, $compileProvider, Config) {
     // Disable debug information when in production to
     // speed up the app's performance.
     if (Config.environment === 'Production') {
@@ -26,4 +26,10 @@ angular.module('resume', [
       .otherwise({
         redirectTo: '/'
       });
+
+    // Use the HTML5 History API. This removes the hash tag from the URL
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
   });
